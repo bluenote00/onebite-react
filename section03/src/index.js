73f4ab686bb268083781d43db67d5f08defadf9c
annej <god0937@gmail.com>
2025-01-16 11:47:09 +0900

    3.5) Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -9,14 +9,19 @@
 //console.log(moduleData.sub(1,2));
 
 // ES6 모듈 형식 호출 - 확장자까지 표기해야함
-import {add, sub} from "./math.js";
+// import {add, sub} from "./math.js";
 
 // ES6의 default 호출 - 중괄호를 뺀다.
-import multiply from "./math.js";
+// import multiply from "./math.js";
 
 // default 함수의 경우 이름을 바꾸는 것도 가능하며, 이렇게 합쳐서 호출하는 것도 가능하다.
 // import mul, {add, sub} from "./math.js";
 
-console.log(add(1,2));
-console.log(sub(1,2));
-console.log(multiply(1,2));
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(multiply(1,2));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
