2a86aff3fa725e497d66083c2fa574715ab787a9
annej <god0937@gmail.com>
2025-01-17 15:14:40 +0900

    5.2) React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Header from "./components/Header.jsx"
+import Main from "./components/Main.jsx"
+import Footer from "./components/Footer.jsx"
 
+// App 컴포넌트
 function App() {
-
   return (
     <>
+    <Header />
+    <Main />
     <h1>안녕 리액트!</h1>
+    <Footer />
     </>
   )
 }
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// Header 컴포넌트
+const Header = () => {
+    return (
+      <header>
+        <h1>header</h1>
+      </header>
+    );
+  }
+
+  export default Header;
