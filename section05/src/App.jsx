85eae10e169ed0c85182eff789be24337e598b05
ara <god0937@gmail.com>
2025-01-19 14:32:13 +0900

    5.8) State로 사용자 입력 관리하기 1

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,51 +1,13 @@
 import './App.css'
-import './components/Bulb'
-import './components/Light'
-import { useState } from "react"
 
-const Bulb = () => {
-   const [light, setLight] = useState("OFF");
-
-  console.log(light);
-  return (
-    <div>
-      {light === 'ON'? 
-      (<h1 style={{backgroundColor:"orange"}}>ON</h1>
-
-      ) : (
-      <h1 style={{backgroundColor:"gray"}}>OFF</h1>
-      )}
-
-      <button onClick={() => {
-        setLight(light === 'ON'? "OFF":"ON")
-       }}
-       >
-        {light ==='ON'? "끄기":"켜기"}
-       </button>      
-     </div>
-  );
-}
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
- return (
-  <div>
-      <h1>{count}</h1>
-      <button 
-        onClick={() => {
-          setCount(count + 1);
-        }}>+
-        </button>
-    </div>
-)}
+import Regsister from "./components/Regsister";
 
 // App 컴포넌트
 function App() {  
  
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Regsister />
     </>
   );
 };
