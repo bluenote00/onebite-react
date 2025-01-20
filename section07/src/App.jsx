f05f968b95fc42f0bd4ce079c89306c36c5ada8d
annej <god0937@gmail.com>
2025-01-20 12:00:30 +0900

    7.3) useEffect로 라이프사이클 제어하기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,16 +1,41 @@
 import "./App.css"
 import Controller from './components/Controller';
 import Viewer from './components/Viewer';
-import { useState, useEffect } from "react";
+import Even from './components/Even';
+
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  
+  const isMount = useRef(false);
+
+  // 1. 마운트 : 탄생
+  // 처음 실행될때만 useEffect를 사용하고싶다면 빈 배열을 넣는다.
+  useEffect(()=>{
+    console.log("mount");
+  }, [])
+
+  // 2. 업데이트 : 변화, 리렌더링
+  // 콜백 함수에서 배열을 아예 제거한다. 화면이 리렌더링 될때마다 갱신됨
+  // 정말 업데이트 할때만 사용하고싶다면, useRef를 사용해 플래그를 세운다.
+  useEffect(()=>{
+    if(!isMount.current){
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  })
+
+  // 3. 언마운트 : 죽음
+
+
 
   // count가 실행될 때마다, 앞에 콜백 함수가 실행되게 된다.
-  useEffect(()=> {
-    console.log(`count : ${count} / input : ${input}`);
-  }, [count, input]);
+  // useEffect(()=> {
+  //   console.log(`count : ${count} / input : ${input}`);
+  // }, [count, input]);
   // 의존성 배열 (dependency arry, deps) 
   // deps에는 값을 여러개 넣어도 된다.
 
@@ -29,6 +54,7 @@ function App() {
         </section>
         <section>
           <Viewer count={count} />
+          {count % 2 === 0? <Even /> : null}
         </section>
         
         <section>
