34a5175986f3e88f0a819b50d21d6e36931d62c3
ara <god0937@gmail.com>
2025-01-25 15:31:26 +0900

    12.4) 페이지 라우팅2

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Link, useNavigate } from "react-router-dom"
 import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
@@ -9,9 +9,20 @@ import NotFound from './pages/NotFound'
 // 2. "/new" : t새로운 일기를 작성하는 new 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new")
+  };
+
   return (
     <>
-      <div>Hello</div>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
