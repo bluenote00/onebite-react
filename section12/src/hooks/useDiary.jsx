427965c7102520cdb99d1788352fe2cc7f708df0
ara <god0937@gmail.com>
2025-01-28 19:50:45 +0900

    12.16) Diary 페이지 구현하기

---
+++ section12/src/hooks/useDiary.jsx
@@ -0,0 +1,27 @@
+import { useContext, useState, useEffect } from "react";
+import { DiaryStateContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
+const useDiary = (id) => {
+    const data = useContext(DiaryStateContext);
+    const [curDiaryItem, setCurDiaryItem] = useState();
+
+    const nav = useNavigate();
+
+    useEffect(() => {
+            const currentDairyItem = data.find(
+                (item) => String(item.id) === String(id)
+            );
+            
+            if (!currentDairyItem) {
+                window.alert("존재하지 않는 일기입니다.")
+                nav("/", { replace : true });
+            }
+            
+            setCurDiaryItem(currentDairyItem);
+        }, [id]);
+
+        return curDiaryItem
+}
+
+export default useDiary;
